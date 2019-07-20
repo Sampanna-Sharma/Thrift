541c4e36c9dec9ade1d10f9e87e835456f4eb9f3
Sampanna-Sharma <073bct538.sampanna@pcampus.edu.np>
2019-07-21 02:20:29 +0545

    sadf

--- scraper/google_cse_scrape.py
+++ scraper/google_cse_scrape.py
@@ -2,9 +2,13 @@ import requests
 import json
 
 key ="AIzaSyAI5RjkaHpSmiRNjrnzsQZImfzAXYLyEO8"
-url = "https://www.googleapis.com/customsearch/v1?key="+key+"&cx=007209971530152036224:dx3jaxmkzfs&cr=countryNP&q=allintitle: buy usb 3.0"
-print(url)
-r =requests.get(url)
-print(r.text)
-# data = json.loads(r.text)
-# links = data.items
+def google_scrape(product_name):
+    response = []
+    url = f"https://www.googleapis.com/customsearch/v1?key={key}&cx=007209971530152036224:dx3jaxmkzfs&cr=countryNP&q=allintitle:{product_name}"
+    print(url)
+    r =requests.get(url)
+    result = json.loads(r.text)
+    for item in result["items"]:
+        response.append(item["link"])
+    
+    return response
