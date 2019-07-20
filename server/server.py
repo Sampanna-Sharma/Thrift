85c5cb90a18264ed5eb1b67a0ccbd013e9b1b7e9
Sampanna-Sharma <073bct538.sampanna@pcampus.edu.np>
2019-07-20 15:32:47 +0545

    dummy result for GR

--- server/server.py
+++ server/server.py
@@ -10,7 +10,10 @@ def search_web():
     link =f"https://www.google.com/search?hl=en&as_q={product_name}+card&as_epq=&as_oq=&as_eq=&as_nlo=&as_nhi=&lr=&cr=countryNP&as_qdr=all&as_sitesearch=&as_occt=title&safe=images&as_filetype=&as_rights="
     r = requests.get(link)
     # result = json.loads(r.text)
-    return str(r.text)
+    result = dict(item = ["item1", "item2", "item3"],
+                url = ["item1", "item2", "item3"],
+                price = ["item1", "item2", "item3"])
+    return jsonify(result)
 
 if __name__ == "__main__":
     app.run(debug = True)
