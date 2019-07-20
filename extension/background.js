cab28108b788737604bc7d85cdf3a24d9512ad73
Sampanna-Sharma <073bct538.sampanna@pcampus.edu.np>
2019-07-20 20:46:30 +0545

    json parsed

--- extension/background.js
+++ extension/background.js
@@ -3,16 +3,23 @@ var text = "";
 chrome.extension.onRequest.addListener(onRequest);
 
 function onRequest(request, sender, sendResponse) {
-   text = "http://google.com/";
-   text = text + request.action.toString();
+   let xhr = new XMLHttpRequest();
+   var product_name=request.action.toString(); 
+   var text= "http://127.0.0.1:5000/?ProductName="+product_name;
+   xhr.open("GET", text);
+   xhr.send();
+  response=[];
+  xhr.onload = function(){
+    var response = xhr.response;
+    response= JSON.parse(response);
+    console.log(response.item[0]);
+  }
 
-   sendResponse({});
 };
-
 
 //create new tab of selected texxt
 chrome.contextMenus.onClicked.addListener(function(tab) {
-  chrome.tabs.create({url:text});
+  chrome.tabs.create({url: text});
 });
 
 //create contextMenu
