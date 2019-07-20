bd76a3c67096c0069355e9eba4654b0b10ac93a6
Sampanna-Sharma <073bct538.sampanna@pcampus.edu.np>
2019-07-20 15:26:05 +0545

    cors removed

--- server/server.py
+++ server/server.py
@@ -1,10 +1,8 @@
 from flask import Flask, jsonify, request
-from flask_cors import CORS
 import requests
 import json
 
 app = Flask(__name__)
-CORS(app)
 
 @app.route('/',methods = ['GET'])
 def search_web():
