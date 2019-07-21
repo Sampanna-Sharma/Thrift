460254ee313f9b10bb33f5fde50f23661e3fb214
Ubuntu <ubuntu@ip-172-31-46-230.ec2.internal>
2019-07-21 06:55:04 +0000

    new api key

--- scraper/google_cse_scrape.py
+++ scraper/google_cse_scrape.py
@@ -1,12 +1,13 @@
 import requests
 import json
 
-key ="AIzaSyAI5RjkaHpSmiRNjrnzsQZImfzAXYLyEO8"
+key ="AIzaSyB-cRSraVQxHE3cEir_ZdPxVcX4osY8aEM"
 def google_scrape(product_name):
     response = []
     url = f"https://www.googleapis.com/customsearch/v1?key={key}&cx=007209971530152036224:dx3jaxmkzfs&cr=countryNP&q=allintitle:{product_name}"
     print(url)
     r =requests.get(url)
+    print(r.text)
     result = json.loads(r.text)
     try:
         for item in result["items"]:
